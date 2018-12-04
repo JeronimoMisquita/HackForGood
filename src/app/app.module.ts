bd07e5ee28644acd0f89d080895020b456ada9a3
cerebral-assasin <jeromemisquita101@gmail.com>
2018-12-04 14:06:56 +0530

    First code push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {DashboardPage} from '../pages/dashboard/dashboard'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RechargeCardPage } from '../pages/recharge-card/recharge-card';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    RechargeCardPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    RechargeCardPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {RechargeCardPage} from '../recharge-card/recharge-card'
+/**
+ * Generated class for the DashboardPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.html',
+})
+export class DashboardPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DashboardPage');
+  }
+
+  goToRecharge(){
+
+
+    this.navCtrl.push(RechargeCardPage);
+
+  }
+
+}
