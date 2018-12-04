b118b313444b148ab1a47d298b730da65160b07c
cerebral-assasin <jeromemisquita101@gmail.com>
2018-12-05 00:16:22 +0530

    Completed QR code page as well as Recharge wallet page

--- src/pages/qr-code-reader/qr-code-reader.ts
+++ src/pages/qr-code-reader/qr-code-reader.ts
@@ -17,7 +17,6 @@ import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
   templateUrl: 'qr-code-reader.html',
 })
 export class QrCodeReaderPage {
-  scannedData = 'None'
   constructor(public platform:Platform,private qrScanner: QRScanner, public navCtrl: NavController, public navParams: NavParams) {
     platform.ready().then(()=>{
                  this.qrscanner();
@@ -25,24 +24,36 @@ export class QrCodeReaderPage {
   }
 
   ionViewDidLoad() {}
+
+despensedWater(){
+
+  this.navCtrl.pop();
+
+}
+
 qrscanner() {
   this.qrScanner.prepare()
   .then((status: QRScannerStatus) => {
      if (status.authorized) {
        // camera permission was granted
-       this.scannedData = 'permission granted'
-
+       window.document.querySelector('#giveWater').setAttribute("style", "visibility:hidden;width:0%;height:0%;");
+       window.document.querySelector('ion-app').setAttribute("style", "visibility:hidden;");
+       window.document.querySelector('ion-app').setAttribute("style", "background: transparent none;");
+       window.document.querySelector('#cameraDisplay').setAttribute("style", "visibility:visible;width:100%;height:auto;");
        // start scanning
        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
          console.log('Scanned something', text);
-          this.scannedData = text
-          alert(text);
+
+          alert("QR code valid !");
 
          this.qrScanner.hide();
          scanSub.unsubscribe();
-         window.document.querySelector('ion-app').classList.remove('transparentBody');
-        // window.document.querySelector('.main-background').classList.remove('transparentBody');
 
+        // window.document.querySelector('.main-background').classList.remove('transparentBody');
+        window.document.querySelector('ion-app').removeAttribute("style");
+        window.document.querySelector('#cameraDisplay').setAttribute("style", "visibility:hidden;height:0;width:0;");
+        window.document.querySelector('ion-app').setAttribute("style", "visibility:visible;");
+        window.document.querySelector('#giveWater').setAttribute("style", "visibility:visible;width:100%;height:100%;");
        });
 
        this.qrScanner.resumePreview();
@@ -50,24 +61,22 @@ qrscanner() {
       // show camera preview
       this.qrScanner.show()
       .then((data : QRScannerStatus)=> {
-         this.scannedData = "showing something status"
         //alert(data.showing);
       },err => {
         //alert(err);
-        this.scannedData = "Err" ;
       });
 
       // wait for user to scan something, then the observable callback will be called
 
 
      } else if (status.denied) {
-         this.scannedData = "permission denied"
+
        // camera permission was permanently denied
        // you must use QRScanner.openSettings() method to guide the user to the settings page
        // then they can grant the permission from there
      } else {
        // permission was denied, but not permanently. You can ask for permission again at a later time.
-       this.scannedData = "permission was denied, but not permanently. You can ask for permission again at a later time"
+
      }
   })
   .catch((e: any) => console.log('Error is', e));
